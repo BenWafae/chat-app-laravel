c57c9b1de140c780a0c84596a62522d99767322e
BenWafae <BENZHIRWAFA@gmail.com>
2025-09-17 13:34:28 +0200

    configuration app.js &bootstap.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
-import './bootstrap';
+import './bootstrap';  // charge tout ce qui est dans bbootstrap.js
 
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,35 +1,39 @@
+/**
+ * bootstrap.js
+ * Configuration initiale du front-end Laravel avec Vite
+ */
+
 import _ from 'lodash';
 window._ = _;
 
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
+ * Axios
+ * Permet d'effectuer facilement des requêtes HTTP vers Laravel
+ * avec gestion automatique du CSRF token
  */
-
 import axios from 'axios';
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
+ * Laravel Echo + Pusher
+ * Permet d'écouter les événements broadcastés en temps réel via WebSockets
  */
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-// import Echo from 'laravel-echo';
+// Rendre Pusher disponible globalement
+window.Pusher = Pusher;
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+// Initialisation de Laravel Echo
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,                   // clé Pusher depuis .env
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',  // cluster Pusher
+    wsHost: import.meta.env.VITE_PUSHER_HOST ?? window.location.hostname, // serveur WebSocket
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,           // port WebSocket
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,          // port WebSocket sécurisé
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'http') === 'https', // HTTPS ?
+    enabledTransports: ['ws', 'wss'],                          // types de transport
+});
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
